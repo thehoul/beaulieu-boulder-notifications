c293ca113a9a09190bcd88e4e9b0ab5bcd93d8f2
HOT <theo@houle.pro>
2025-12-13 14:11:02 +0100

    improve logging format

--- util/logging.py
+++ util/logging.py
@@ -4,7 +4,7 @@ LOGGING_FILE = "logging.log"
 
 logging.basicConfig(filename=LOGGING_FILE,
     filemode='a',
-    format='%(asctime)s,%(msecs)03d %(name)s %(levelname)s %(message)s',
+    format='[%(asctime)s][%(name)s] %(levelname)s: %(message)s',
     datefmt='%Y-%m-%d %H:%M:%S',
     level=logging.INFO)
 
